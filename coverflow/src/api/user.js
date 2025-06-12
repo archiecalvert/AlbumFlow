44b7dbe155fc3d4a2908c5f8d9c43ab11fb31572
Archie Calvert <archie.calvert@student.manchester.ac.uk>
2025-06-12 20:19:40 +0100

    Added to new branch for heroku

--- coverflow/src/api/user.js
+++
@@ -1,4 +0,0 @@
-export async function GetUserDetails(access_token)
-{
-    
-}
--- server/helpers.js
+++ server/helpers.js
@@ -1,3 +1,11 @@
+/*
+
+Function to generate the state value for authentication.
+Generates a random string that is used for additional authentication when getting authentication code.
+
+(from Spotify Web API Docs)
+
+*/
 const generateRandomString = (length) => {
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const values = crypto.getRandomValues(new Uint8Array(length));
